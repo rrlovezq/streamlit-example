ca43dbf48815f5262d758f606ce82029e461ae69
yubin <142982@qq.com>
2023-10-24 17:59:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,50 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+
+###############
+# Authored by Weisheng Jiang
+# Book 3  |  From Basic Arithmetic to Machine Learning
+# Published and copyrighted by Tsinghua University Press
+# Beijing, China, 2022
+###############
+
+# Streamlit_Bk3_Ch1_02.py
+
+from mpmath import mp
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
-"""
-# Welcome to Streamlit!
+with st.sidebar:
+    
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+    num_digits = st.slider('Number of decimal digits:',
+                           min_value = 10000, 
+                           max_value = 100000,
+                           step = 10000)
+    
+mp.dps = num_digits + 2
+pi_digits = mp.pi
+# st.write(str(pi_digits))
+pi_digits = str(pi_digits)[2:]
+pi_digits_list = [int(x) for x in pi_digits]
+# st.write(pi_digits_list)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+pi_digits_array = np.array(pi_digits_list)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+counts = np.bincount(pi_digits_array)
+
+fig, ax = plt.subplots()
+
+ax.barh(range(10), counts, align = 'center',
+        edgecolor = [0.6, 0.6, 0.6])
+
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+ax.set_xlabel('Count')
+ax.set_ylabel('Digit, 0~9')
+plt.yticks(range(10))
+
+st.pyplot(fig)
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    
